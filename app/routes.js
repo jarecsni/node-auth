edd69037d682a0656bd2cac33a3187cc89960937
Janos Jarecsni <janos.jarecsni@gmail.com>
2014-12-16 18:38:01 +0000

    Added Twitter authentication - Tutorial 3 http://scotch.io/tutorials/javascript/easy-node-authentication-twitter

--- app/routes.js
+++ app/routes.js
@@ -43,6 +43,20 @@ module.exports = function(app, passport) {
         res.redirect('/');
     });
 
+    // =====================================
+    // TWITTER ROUTES ======================
+    // =====================================
+    // route for twitter authentication and login
+    app.get('/auth/twitter', passport.authenticate('twitter'));
+
+    // handle the callback after twitter has authenticated the user
+    app.get('/auth/twitter/callback',
+        passport.authenticate('twitter', {
+            successRedirect : '/profile',
+            failureRedirect : '/'
+        }));
+
+
 
     // =====================================
     // SIGNUP ==============================
--- config/auth.js
+++ config/auth.js
@@ -1,5 +1,6 @@
 module.exports = {
 
+    // TODO hosts and ports are hardcoded, needs fixing
     'facebookAuth' : {
         'clientID'      : '341567182697899', // your App ID
         'clientSecret'  : '3c2b03ee1e763e9ff5ecb3370cae1ed3', // your App Secret
@@ -7,9 +8,9 @@ module.exports = {
     },
 
     'twitterAuth' : {
-        'consumerKey'       : 'your-consumer-key-here',
-        'consumerSecret'    : 'your-client-secret-here',
-        'callbackURL'       : 'http://localhost:8080/auth/twitter/callback'
+        'consumerKey'       : 'fJF5KbO7TpJAM3XunLbeu9zUl',
+        'consumerSecret'    : 'W4Xp5XOXlFsAE9JHZ8m3beFsztPj2J9Z9K4G4j0PeTx1oMzvBf',
+        'callbackURL'       : 'http://localhost:3000/auth/twitter/callback'
     },
 
     'googleAuth' : {
