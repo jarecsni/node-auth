10b223204a3770a0c7cff533d50c5f44a8f7bc62
Janos Jarecsni <janos.jarecsni@gmail.com>
2014-12-16 18:05:29 +0000

    Added Facebook authentication - Tutorial 2 http://scotch.io/tutorials/javascript/easy-node-authentication-facebook

--- app/routes.js
+++ app/routes.js
@@ -25,6 +25,26 @@ module.exports = function(app, passport) {
 
 
     // =====================================
+    // FACEBOOK ROUTES =====================
+    // =====================================
+    // route for facebook authentication and login
+    app.get('/auth/facebook', passport.authenticate('facebook', { scope : 'email' }));
+
+    // handle the callback after facebook has authenticated the user
+    app.get('/auth/facebook/callback',
+        passport.authenticate('facebook', {
+            successRedirect : '/profile',
+            failureRedirect : '/'
+        }));
+
+    // route for logging out
+    app.get('/logout', function(req, res) {
+        req.logout();
+        res.redirect('/');
+    });
+
+
+    // =====================================
     // SIGNUP ==============================
     // =====================================
     // show the signup form
@@ -60,7 +80,7 @@ module.exports = function(app, passport) {
         req.logout();
         res.redirect('/');
     });
-};
+}
 
 // route middleware to make sure a user is logged in
 function isLoggedIn(req, res, next) {
---
+++ config/auth.js
@@ -0,0 +1,21 @@
+module.exports = {
+
+    'facebookAuth' : {
+        'clientID'      : '341567182697899', // your App ID
+        'clientSecret'  : '3c2b03ee1e763e9ff5ecb3370cae1ed3', // your App Secret
+        'callbackURL'   : 'http://localhost:3000/auth/facebook/callback'
+    },
+
+    'twitterAuth' : {
+        'consumerKey'       : 'your-consumer-key-here',
+        'consumerSecret'    : 'your-client-secret-here',
+        'callbackURL'       : 'http://localhost:8080/auth/twitter/callback'
+    },
+
+    'googleAuth' : {
+        'clientID'      : 'your-secret-clientID-here',
+        'clientSecret'  : 'your-client-secret-here',
+        'callbackURL'   : 'http://localhost:8080/auth/google/callback'
+    }
+
+};
